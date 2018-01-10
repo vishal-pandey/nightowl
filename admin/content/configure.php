24fe95289c52d36a67aef39cb011e822f792f30e
vishal-pandey <pandeyvishal64742@gmail.com>
2018-01-10 19:07:32 +0530

    database update

--- admin/content/configure.php
+++ admin/content/configure.php
@@ -1,7 +1,7 @@
 <?php
 	$server = "localhost";
-	$user = "niteowl";
+	$user = "niteowl1";
 	$pwd = "Pandey@123";
-	$db = "niteowl";
+	$db = "niteowl1";
 	$conn = new mysqli($server, $user, $pwd, $db);
 ?>
